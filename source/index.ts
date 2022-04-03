cfac085a902ce2b167e2b7bb445cda7ba37be370
euberdeveloper <euberdeveloper@gmail.com>
2022-04-03 14:22:58 +0200

    feat: publish new version
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/index.ts
+++ source/index.ts
@@ -1,11 +1,2 @@
-// export * from '@/types';
-// export { GifAnalyzer } from '@/utils/analyzer';
-
-import * as moduleAlias from 'module-alias';
-import * as path from 'path';
-import * as fs from 'fs';
-moduleAlias.addAlias('@', path.join(process.cwd(), 'dist'));
-import { GifAnalyzer } from '@/utils/analyzer';
-const gif = new GifAnalyzer(fs.readFileSync('./test.gif'));
-console.log(JSON.stringify(gif.value, null, 2));
-console.log(JSON.stringify(gif.raw, null, 2));
+export * from '@/types';
+export { GifAnalyzer } from '@/utils/analyzer';
